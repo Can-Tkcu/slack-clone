df6028ca9b1c38a9364ce9f8747a47e8ae6a86a6
HanamiGeto <tidu11@hotmail.de>
2023-04-19 16:41:05 +0200

    import button module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu'; 
+import { MatMenuModule } from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button'; 
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, HomeComponent, HomeHeaderComponent],
+  declarations: [
+    AppComponent, 
+    LoginComponent, 
+    HomeComponent, 
+    HomeHeaderComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatMenuModule
+    MatMenuModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
