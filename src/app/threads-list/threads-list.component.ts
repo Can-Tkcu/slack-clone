be5ad34c69bf20c53c6c8d569f3ca011fd69e3b5
cantkcu <can.tkcu@hotmail.com>
2023-05-09 12:03:55 +0200

    added logo

--- src/app/threads-list/threads-list.component.ts
+++ src/app/threads-list/threads-list.component.ts
@@ -5,12 +5,11 @@ import { ChannelService } from '../services/channel.service';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { where } from 'firebase/firestore';
 
-
 @UntilDestroy()
 @Component({
   selector: 'app-threads-list',
   templateUrl: './threads-list.component.html',
-  styleUrls: ['./threads-list.component.scss']
+  styleUrls: ['./threads-list.component.scss'],
 })
 export class ThreadsListComponent implements OnInit {
   allChannels: any = [];
@@ -22,42 +21,39 @@ export class ThreadsListComponent implements OnInit {
     private firestore: AngularFirestore,
     public usersService: UsersService,
     public channelService: ChannelService
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.firestore
-    .collection('channels')
-    .valueChanges({idField: 'channelId'})
-    .pipe(untilDestroyed(this))
-    .subscribe((channels: any) => {
-      // console.log(channels)
-      this.allChannels = channels;
-      this.getOwnThreads();
-    })
+      .collection('channels')
+      .valueChanges({ idField: 'channelId' })
+      .pipe(untilDestroyed(this))
+      .subscribe((channels: any) => {
+        // console.log(channels)
+        this.allChannels = channels;
+        this.getOwnThreads();
+      });
   }
 
   getOwnThreads() {
     this.ownThreads = [];
     // this.ownChannels = [];
-    this.allChannels.forEach((e, i)  => {
-      e.thread?.forEach(element => {
+    this.allChannels.forEach((e) => {
+      e.thread?.forEach((element) => {
         // console.log(element)
         if (element.author == this.usersService.currentUserDataID) {
           // console.log(e)
           this.ownThreads.push({
             elem: element,
-            name: e.name
+            name: e.name,
           });
           // console.log(this.ownThreads)
           // this.channelNames.push(e.name);
           this.ownThreads.sort(function (x, y) {
-            
-            return y.timestamp - x.timestamp;
+            return y.elem.timestamp - x.elem.timestamp;
           });
-
         }
       });
-      
     });
   }
 }
